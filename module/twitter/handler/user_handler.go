681c3d2611ee720d752695dd8a540032b3ef0caa
rema424 <rema.gapp@gmail.com>
2019-06-08 11:11:32 +0900

    add: 並行処理

--- module/twitter/handler/user_handler.go
+++ module/twitter/handler/user_handler.go
@@ -1,11 +1,18 @@
 package handler
 
 import (
+	"context"
 	"database/sql"
 	"net/http"
-	"strconv"
+
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+
+	// "google.golang.org/appengine/memcache"
 
 	"github.com/labstack/echo"
+	"github.com/rema424/twitter-reproduction-backend/model"
 	"github.com/rema424/twitter-reproduction-backend/service"
 )
 
@@ -36,13 +43,44 @@ func (h *userHandler) Index(c echo.Context) error {
 }
 
 func (h *userHandler) Show(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	u, err := h.userService.Get(id)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return c.JSON(http.StatusOK, "データが見つかりませんでした。")
-		}
-		return c.JSON(http.StatusOK, "予期せぬエラーが発生しました。")
+	// id, _ := strconv.Atoi(c.Param("id"))
+	// u, err := h.userService.Get(id)
+	// if err != nil {
+	// 	if err == sql.ErrNoRows {
+	// 		return c.JSON(http.StatusOK, "データが見つかりませんでした。")
+	// 	}
+	// 	return c.JSON(http.StatusOK, "予期せぬエラーが発生しました。")
+	// }
+
+	eg, ctx := errgroup.WithContext(getContext(c))
+
+	var u1, u2, u3 *model.User
+	eg.Go(func() (err error) {
+		u1, err = h.userService.Get(11)
+		return
+	})
+	eg.Go(func() (err error) {
+		u2, err = h.userService.Get(2)
+		return
+	})
+	eg.Go(func() (err error) {
+		u3, err = h.userService.Get(3)
+		return
+	})
+
+	if err := eg.Wait(); err != nil {
+		log.Errorf(ctx, "error: %s", err)
 	}
-	return c.JSONPretty(http.StatusOK, u, "  ")
+
+	data := map[string]interface{}{
+		"u1": u1,
+		"u2": u2,
+		"u3": u3,
+	}
+
+	return c.JSONPretty(http.StatusOK, data, "  ")
+}
+
+func getContext(c echo.Context) context.Context {
+	return appengine.NewContext(c.Request())
 }
